cdddfb792bcfe52165551f6d9ff34b70fe9dc920
Sharon <sharonelizalex@gmail.com>
2021-05-16 22:38:17 +1000

    updated index.js

--- index.js
+++ index.js
@@ -8,14 +8,12 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.static('public'));
 
-//routes
+//Routes
 
-//apiRoutes
-require('./routes/apiRoutes')(app);
+require('./routes/apiRoutes')(app);  //ApiRoutes
+require('./routes/htmlRoutes')(app); //HtmlRoutes
 
-//htmlRoutes
-require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, ()=>{
-    console.log(`server up and running on port ${PORT}`);
+    console.log(`server up and running on port: ${PORT}`);
 });
