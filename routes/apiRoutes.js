9d9a1885ab18ac312fd76f482887c6967afc66d3
Sharon <sharonelizalex@gmail.com>
2021-05-17 11:32:47 +1000

    updated apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,22 @@
 const { uuid } = require('uuidv4');
 const fs = require('fs');
+const { response } = require('express');
 
-module.exports = function(app){
+module.exports = (app)=>{
 
     app.get("/api/notes", (req, res)=>{
-        console.log("GET notes...")
-        //read the db.json file
-        // display all notes in response body.
+        console.log("GET notes...");
+        let noteData = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        response.json(noteData);
     })
 
     app.post("/api/notes",(req, res)=>{
-        console.log("note created")
-
+        console.log("Note created...");
+        const newNote = req.body;
+        newNote['id'] = uuid();
+        let noteData = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        noteData.push(newNote);
+        res.json(noteData);
         //get note from Request body.
         //write to file after assigninng uniq id
 
